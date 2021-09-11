02d6c9f0ce7c122151507676b8c6ad3da2202cab
Adrien Martin <adrien.v.martin@gmail.com>
2021-09-10 23:12:19 -0600

    Declare routeHandler variable

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,3 +1,7 @@
 export function controller(routePrefix: string) {
-  return function (target: Function) {};
+  return function (target: Function) {
+    for (let key in target.prototype) {
+      const routeHandler = target.prototype[key];
+    }
+  };
 }
